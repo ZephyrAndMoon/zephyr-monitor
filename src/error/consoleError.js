dc056645da8e67251533a13bdc5819548137066a
zephyr <1024676057@qq.com>
2021-05-17 15:47:29 +0800

    refactor: 修改打印错误方法名

--- src/error/consoleError.js
+++ src/error/consoleError.js
@@ -1,10 +1,9 @@
-import BaseMonitor from "../base/baseMonitor.js";
-import { ErrorCategoryEnum, ErrorLevelEnum } from "../base/baseConfig.js"
+import BaseMonitor from '../base/baseMonitor.js';
+import { ErrorCategoryEnum, ErrorLevelEnum } from '../base/baseConfig.js';
 /**
  * console.error异常
  */
 class ConsoleError extends BaseMonitor {
-
     constructor(params) {
         super(params);
     }
@@ -12,7 +11,7 @@ class ConsoleError extends BaseMonitor {
     /**
      * 处理console事件
      */
-    handleError () {
+    handleError() {
         this.registerInfo();
         this.registerWarn();
         this.registerError();
@@ -21,65 +20,64 @@ class ConsoleError extends BaseMonitor {
     /**
      * 处理信息
      */
-    registerInfo () {
+    registerInfo() {
         let t = this;
-        console.tInfo = function () {
+        console.Info = function () {
             t.handleLog(ErrorLevelEnum.INFO, ErrorCategoryEnum.CONSOLE_INFO, arguments);
-        }
+        };
     }
 
     /**
      * 处理警告
      */
-    registerWarn () {
+    registerWarn() {
         let t = this;
-        console.tWarn = function () {
+        console.Warn = function () {
             t.handleLog(ErrorLevelEnum.WARN, ErrorCategoryEnum.CONSOLE_WARN, arguments);
-        }
+        };
     }
 
     /**
      * 处理错误
      */
-    registerError () {
+    registerError() {
         let t = this;
-        console.tError = function () {
+        console.Error = function () {
             t.handleLog(ErrorLevelEnum.ERROR, ErrorCategoryEnum.CONSOLE_ERROR, arguments);
-        }
+        };
     }
 
     /**
      * 处理日志
      */
-    handleLog (level, category, args) {
+    handleLog(level, category, args) {
         try {
             this.level = level;
             let params = [...args];
-            this.msg = params.join("\r\n"); //换行符分割
-            this.url = location.href;   //当前地址
+            this.msg = params.join('\r\n'); //换行符分割
+            this.url = location.href; //当前地址
             this.category = category;
             this.recordError();
         } catch (error) {
-            console.log("console统计错误异常", level, error);
+            console.log('console统计错误异常', level, error);
         }
     }
-
 }
 
 /**
  * 初始化console事件
  */
 (function () {
-    //创建空console对象，避免JS报错  
+    //创建空console对象，避免JS报错
     if (!window.console) {
         window.console = {};
     }
     let funcs = ['tInfo', 'tWarn', 'tError'];
     funcs.forEach((func, index) => {
         if (!console[func]) {
-            console[func] = function () { };
+            console[func] = function () {};
         }
     });
-})()
+})();
 
 export default ConsoleError;
