131def9e63963e1b6519a855542bb73022a18f7e
zephyr <1024676057@qq.com>
2021-05-18 15:34:15 +0800

    refactor: 修改测试组件名

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Type msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Type from './components/Type.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Type
   }
 }
 </script>
