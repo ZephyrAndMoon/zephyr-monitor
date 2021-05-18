0d3654f6359bfee540c5b541733f2d946ec22cca
zephyr <1024676057@qq.com>
2021-05-18 15:27:31 +0800

    feat: 修改服务端路由

--- example/server/app.js
+++ example/server/app.js
@@ -2,7 +2,7 @@ const Koa = require('koa2');
 const app = new Koa();
 const cor = require('./utils/cor');
 const { logger, accessLogger } = require('./utils/log');
-const Router = require('koa-router');
+const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 
 const post = 3000;
@@ -14,25 +14,21 @@ app.use(bodyParser());
 // 配置日志
 app.use(accessLogger());
 
-let index = new Router();
+
 
 // 子路由1
-index.post('/', async ctx => {
+router.get('/', async ctx => {
     ctx.body = 'Server Status Ready';
 });
 
 // 子路由1
-index.post('/mointor', async ctx => {
+router.post('/monitor', async ctx => {
     logger.info(ctx.request.body);
     ctx.body = 'Already recorded parameters';
 });
 
-// 装载所有子路由
-let router = new Router();
-router.use('/', index.routes(), index.allowedMethods());
-
-// 加载路由中间件
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes());   /*启动路由*/
+app.use(router.allowedMethods());
 
 app.listen(post, () => {
     console.log(`server is starting at port ${post}`);
