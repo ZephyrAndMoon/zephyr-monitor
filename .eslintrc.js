123ff33bf12ddfe2d37bce5c432210f498b4076d
zephyr <zephyrandmoon@gmail.com>
2021-09-01 23:19:47 +0800

    chore(deps): 集成 Commitizen 与 commitlint 校验 git 提交规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,17 @@ module.exports = {
         browser: true,
         commonjs: true,
         es2021: true,
-        node: true
+        node: true,
     },
     extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended'],
     parserOptions: {
-        ecmaVersion: 12
+        ecmaVersion: 12,
     },
     rules: {
         'no-console': 'off',
         'no-extend-native': 'off',
         'no-underscore-dangle': 'off',
         'no-restricted-globals': 'off',
-        'class-methods-use-this': 'off'
-    }
+        'class-methods-use-this': 'off',
+    },
 }
