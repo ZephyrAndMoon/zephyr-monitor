573efdfd427281f5839a01119c6ab94b01927ff8
zephyr <zephyrandmoon@gmail.com>
2021-09-01 16:24:54 +0800

    fix: 修改属性导出方式

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import FrontEndMonitor from './monitor.js'
-export { FrontEndMonitor }
+import FrontEndMonitor from './monitor'
+
+export default FrontEndMonitor
 
 window.FEMonitor = FrontEndMonitor
